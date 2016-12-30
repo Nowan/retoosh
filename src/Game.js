91e72c2870210578c1f7aea7490575e210eb2656
mmerazmus <maksymilian.erazmus@gmail.com>
2016-12-30 11:43:28 +0100

    Dodanie i automatyczne przesuwanie tła, dodanie przeciwników i funkcji do ich generowania (#21), dodanie scoringu który jest ujemny gdy wpadamy na przeciwników (#12)

--- src/Game.js
+++ src/Game.js
@@ -1,14 +1,19 @@
 Screen.Game = function(game) {};
 Screen.Game.prototype = {
+
     create: function() {
+
+        previousTimestamp = new Date();
+        currentTimestamp = new Date();
+
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        ball = this.game.add.sprite(50, 50, 'ball');
+        background = this.game.add.tileSprite(0, 0, Screen.WIDTH, Screen.HEIGHT, 'background');
 
-        this.game.physics.enable(ball, Phaser.Physics.ARCADE);
-        ball.body.velocity.set(150,150);
-        ball.body.collideWorldBounds = true;
-        ball.body.bounce.set(1);
+        background.autoScroll(-40, 0);
+
+        score = 0;
+        scoreText = this.game.add.text(10, 10, 'Score: 0', { font: '40px Phosphate', fill: '#ffffff' });
 
         spaceship = this.game.add.sprite(this.game.world.width * 0.5, this.game.world.height * 0.5, 'spaceship');
         this.game.physics.enable(spaceship, Phaser.Physics.ARCADE);
@@ -16,11 +21,56 @@ Screen.Game.prototype = {
 
         spaceship.body.collideWorldBounds = true;
         spaceship.body.immovable = true;
+
+        enemies = this.game.add.group();
     },
+
     update: function() {
-        this.game.physics.arcade.collide(ball, spaceship);
+        currentTimestamp = new Date();
+
+        // this.game.physics.arcade.collide(enemies);
+        this.game.physics.arcade.collide(spaceship, enemies, this.enemyHitPlayer, null, null);
+
+        console.log(previousTimestamp);
+        console.log(currentTimestamp);
+        if((currentTimestamp - previousTimestamp) / 1000 > 2){
+            var noEnemies = Math.floor((Math.random() * 20) + 1);
+            for(i=0; i< noEnemies; i++) {
+                this.generateEnemies();
+            }
+            previousTimestamp = currentTimestamp;
+        }
 
         spaceship.x = this.game.input.x || this.game.world.width * 0.5;
         spaceship.y = this.game.input.y || this.game.world.height * 0.5;
     },
+ 
+    enemyHitPlayer: function (spaceship, enemy) {
+        enemy.kill();
+        score -= 100;
+        scoreText.setText('Score: ' + score);
+    },
+
+    playerKillEnemy: function () {
+
+    },
+
+    generateEnemies: function () {
+        var newEnemyHeight = Math.floor((Math.random() * Screen.HEIGHT) + 1);
+        var newEnemyVelocity = Math.floor((Math.random() * 300) + 80);
+
+        newEnemy = this.game.add.sprite(1000, newEnemyHeight, 'enemy');
+
+        var rand = this.game.rnd.realInRange(0.8, 1.5);
+
+        newEnemy.scale.setTo(rand, rand);
+
+        this.game.physics.enable(newEnemy, Phaser.Physics.ARCADE);
+
+        newEnemy.body.velocity.set(newEnemyVelocity*(-1), 0);
+        newEnemy.body.collideWorldBounds = false;
+
+        enemies.add(newEnemy);
+    }
+    
 };
--- src/Preloader.js
+++ src/Preloader.js
@@ -4,11 +4,14 @@ Screen.Preloader.prototype = {
   preload: function () {
       this.game.stage.backgroundColor = '#eee';
 
+      this.game.load.image('background', 'img/Space.png');
+
+      this.game.load.image('enemy', 'img/enemy.png');
       this.game.load.image('ball','img/ball.png');
-      this.game.load.image('spaceship', 'img/spaceship.png');;;;;;;;;;;;;
+      this.game.load.image('spaceship', 'img/spaceship.png');
 
       this.game.load.image('title', 'img/title.png');
-      this.game.load.image('screen-howtoplay', 'img/screen-howtoplay.png');;;;;;;;;;;;;
+      this.game.load.image('screen-howtoplay', 'img/screen-howtoplay.png');
 
       this.game.load.spritesheet('button-start', 'img/button-start.png', 146, 51);
   },
